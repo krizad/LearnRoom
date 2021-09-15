0d2435e383194c3b16371fc6289b249777412986
Kridsada Inthason <vim_intason@hotmail.com>
2021-09-15 22:25:09 +0700

    init

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-require("dotenv").config();
 const express = require('express')
 const app = express()
-    // const cors = require('cors')
-    // app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-    // const { ExpressPeerServer } = require('peer');
-    // const peerServer = ExpressPeerServer(server, {
-    //     debug: true
-    // });
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
 const { v4: uuidV4 } = require('uuid')
 
-// app.use('/peerjs', peerServer);
+app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))

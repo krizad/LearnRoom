27019699d2beb62f0d8d27982a030f5641afa240
Kridsada Inthason <vim_intason@hotmail.com>
2021-09-15 18:45:58 +0700

    init

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host: 'lms-zoom-clone.herokuapp.com',
+    host: '/',
     port: '443'
 })
 const myVideo = document.createElement('video')

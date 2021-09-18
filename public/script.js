ea5295bd27519bae25d1201bdfa92206cf863487
Kridsada Inthason <vim_intason@hotmail.com>
2021-09-19 01:44:18 +0700

    init

--- public/script.js
+++ public/script.js
@@ -89,7 +89,7 @@ const muteUnmute = () => {
 }
 
 const playStop = () => {
-    console.log('object')
+    // console.log('object')
     let enabled = myVideoStream.getVideoTracks()[0].enabled;
     if (enabled) {
         myVideoStream.getVideoTracks()[0].enabled = false;
@@ -103,7 +103,7 @@ const playStop = () => {
 const setMuteButton = () => {
     const html = `
     <i class="fas fa-microphone"></i>
-    <span>Mute</span>
+    <span></span>
   `
     document.querySelector('.main__mute_button').innerHTML = html;
 }
@@ -111,7 +111,7 @@ const setMuteButton = () => {
 const setUnmuteButton = () => {
     const html = `
     <i class="unmute fas fa-microphone-slash"></i>
-    <span>Unmute</span>
+    <span></span>
   `
     document.querySelector('.main__mute_button').innerHTML = html;
 }
@@ -119,7 +119,7 @@ const setUnmuteButton = () => {
 const setStopVideo = () => {
     const html = `
     <i class="fas fa-video"></i>
-    <span>Stop Video</span>
+    <span></span>
   `
     document.querySelector('.main__video_button').innerHTML = html;
 }
@@ -127,7 +127,7 @@ const setStopVideo = () => {
 const setPlayVideo = () => {
     const html = `
   <i class="stop fas fa-video-slash"></i>
-    <span>Play Video</span>
+    <span></span>
   `
     document.querySelector('.main__video_button').innerHTML = html;
 }

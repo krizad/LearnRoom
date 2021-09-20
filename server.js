8aff9568b201f504c8705573f6e5317be2a128d6
Kridsada Inthason <vim_intason@hotmail.com>
2021-09-21 01:18:05 +0700

    make it better

--- server.js
+++ server.js
@@ -4,13 +4,6 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
-const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-    debug: true
-});
-
-app.use('/peerjs', peerServer);
-
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))

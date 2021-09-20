48dd007d5a75dec837ba622dcd70ebe939179046
Kridsada Inthason <vim_intason@hotmail.com>
2021-09-20 21:44:41 +0700

    init

--- public/script.js
+++ public/script.js
@@ -1,9 +1,12 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-    host: 'lms-peer-server.herokuapp.com',
-    port: '443'
-})
+
+
+// const myPeer = new Peer(undefined, {
+//     host: 'lms-peer-server.herokuapp.com',
+//     port: '443'
+// })
+const myPeer = new Peer(undefined, {})
 let myVideoStream;
 const myVideo = document.createElement('video')
 myVideo.muted = true;

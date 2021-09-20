808dd1b716e96999306b9385fce8c1fb6bc8669c
Kridsada Inthason <vim_intason@hotmail.com>
2021-09-20 20:14:35 +0700

    init

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.use('/peerjs', peerServer);
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'learn.sj-assist.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain)
+
 app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
 })

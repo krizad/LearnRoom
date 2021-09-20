03b732794b61ed24c08ae11b62a753cc28db237d
Kridsada Inthason <vim_intason@hotmail.com>
2021-09-20 23:18:39 +0700

    init

--- public/script.js
+++ public/script.js
@@ -10,6 +10,7 @@ let myVideoStream;
 const myVideo = document.createElement('video')
 myVideo.muted = true;
 const peers = {}
+var name;
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
@@ -41,7 +42,8 @@ navigator.mediaDevices.getUserMedia({
     $('html').keydown(function(e) {
         if (e.which == 13 && text.val().length !== 0) {
             socket.emit('message', text.val());
-            $(".chat-area").append(`<div class="message-wrapper"><div class="profile-picture"><img src="https://images.unsplash.com/photo-1581824283135-0666cf353f35?ixlib=rb-1.2.1&auto=format&fit=crop&w=1276&q=80" alt="pp"></div><div class="message-content"><p class="name">Ryan Patrick</p><div class="message">${text.val()}</div></div></div>`);
+            $(".chat-area").append(`<div class="message-wrapper reverse"><p class="name">` +
+                name + `</p><div class="message">${text.val()}</div></div></div>`);
             scrollToBottom()
             text.val('')
         }
@@ -62,7 +64,7 @@ navigator.mediaDevices.getUserMedia({
 
 
 myPeer.on('open', id => {
-    var name = prompt("What's your name?")
+    name = prompt("What's your name?")
     socket.emit('join-room', ROOM_ID, id, name)
 })
 
